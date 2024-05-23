ac7cc23e4c5792df9bea7479979561fe1deaf46c
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2024-05-23 10:51:49 -0700

    Working Draft

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,35 +2,35 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-// (function() {
-//     const moonContainer = document.getElementById('moonContainer');
-//     const moonPhases = ['🌕', '🌔', '🌓', '🌒', '🌑', '🌑', '🌘', '🌗', '🌖', '🌕'];
-//     for (let index = 0; index < moonPhases.length; index++) {
-//         const moon = moonPhases[index];
-//         // for each moon, create a div with class phase and id moonPhase
-//         const moonDiv = document.createElement('div');
-//         moonDiv.setAttribute('class', 'phase');
-//         moonDiv.setAttribute('id', 'moonPhase'+index);
-//         // save that div to a const and put it in the moonContainer
-//         const moonContent = moonContainer.appendChild(moonDiv);
-//         // display each moon on the DOM
-//         moonContent.innerHTML = moon;
-//         moonContent.style.opacity = '0';
-//         let lastMoonIndex = index - 1;
-//         if(index-1 < 0) {
-//             lastMoonIndex = moonPhases.length - 1;
-//         }
-//         setTimeout(function() {
-//             const lastMoon = document.getElementById('moonPhase'+lastMoonIndex);
-//             lastMoon.style.opacity = '0';
-//             moonContent.style.opacity = '1';
-//             setInterval(function() {
-//                 moonContent.style.opacity = '1';
-//                 lastMoon.style.opacity = '0';
-//             }, moonPhases.length*200);
-//         }, 200*index);
-//     }
-// })()
+(function() {
+    const moonContainer = document.getElementById('moonContainer');
+    const moonPhases = ['🌕', '🌔', '🌓', '🌒', '🌑', '🌑', '🌘', '🌗', '🌖', '🌕'];
+    for (let index = 0; index < moonPhases.length; index++) {
+        const moon = moonPhases[index];
+        // for each moon, create a div with class phase and id moonPhase
+        const moonDiv = document.createElement('div');
+        moonDiv.setAttribute('class', 'phase');
+        moonDiv.setAttribute('id', 'moonPhase'+index);
+        // save that div to a const and put it in the moonContainer
+        const moonContent = moonContainer.appendChild(moonDiv);
+        // display each moon on the DOM
+        moonContent.innerHTML = moon;
+        moonContent.style.opacity = '0';
+        let lastMoonIndex = index - 1;
+        if(index-1 < 0) {
+            lastMoonIndex = moonPhases.length - 1;
+        }
+        setTimeout(function() {
+            const lastMoon = document.getElementById('moonPhase'+lastMoonIndex);
+            lastMoon.style.opacity = '0';
+            moonContent.style.opacity = '1';
+            setInterval(function() {
+                moonContent.style.opacity = '1';
+                lastMoon.style.opacity = '0';
+            }, moonPhases.length*200);
+        }, 200*index);
+    }
+})()
 function Nbar() {
   
     return (
