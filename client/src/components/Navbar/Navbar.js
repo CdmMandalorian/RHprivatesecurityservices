0a24779b2d03982ae3ff968e39cb227b67d83e67
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2024-05-24 09:09:34 -0700

    moon-comp

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,36 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { MoonPhase } from 'moonphases-react';
 
-(function() {
-    const moonContainer = document.getElementById('moonContainer');
-    const moonPhases = ['🌕', '🌔', '🌓', '🌒', '🌑', '🌑', '🌘', '🌗', '🌖', '🌕'];
-    for (let index = 0; index < moonPhases.length; index++) {
-        const moon = moonPhases[index];
-        // for each moon, create a div with class phase and id moonPhase
-        const moonDiv = document.createElement('div');
-        moonDiv.setAttribute('class', 'phase');
-        moonDiv.setAttribute('id', 'moonPhase'+index);
-        // save that div to a const and put it in the moonContainer
-        const moonContent = moonContainer.appendChild(moonDiv);
-        // display each moon on the DOM
-        moonContent.innerHTML = moon;
-        moonContent.style.opacity = '0';
-        let lastMoonIndex = index - 1;
-        if(index-1 < 0) {
-            lastMoonIndex = moonPhases.length - 1;
-        }
-        setTimeout(function() {
-            const lastMoon = document.getElementById('moonPhase'+lastMoonIndex);
-            lastMoon.style.opacity = '0';
-            moonContent.style.opacity = '1';
-            setInterval(function() {
-                moonContent.style.opacity = '1';
-                lastMoon.style.opacity = '0';
-            }, moonPhases.length*200);
-        }, 200*index);
-    }
-})()
 function Nbar() {
   
     return (
@@ -49,7 +21,7 @@ function Nbar() {
               <Nav.Link href="/contact">CONTACT</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-          <div class="moonContainer" id="moonContainer"></div>
+          <MoonPhase />
         </Container>
       </Navbar>
     );
